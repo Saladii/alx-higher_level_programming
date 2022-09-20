5058bc4dca64ace04bc61d8b69953d8d5d668008
selehadin <selehadinseid1@gmail.com>
2022-09-20 07:52:42 -0700

    update

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -3,4 +3,18 @@
 
 #include <stdlib.h>
 
+/**
+ * struct listint_s -singly linked list
+ * @n: integer
+ * @next: points to the next code
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 #endif

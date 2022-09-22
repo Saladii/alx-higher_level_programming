a4af004c9c9d6873c9144836c5b7894fa41d6692
selehadin <selehadinseid1@gmail.com>
2022-09-22 07:44:33 -0700

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     special_chars = " ,-_@^*&%$!"
     for c in str:
         ascii = ord(c)
-        if ascii >= 97:
+        if ascii >= 97 and ascii <= 122:
             ascii = ascii - 32
             print("{0:c}".format(ascii), end="")
     print()

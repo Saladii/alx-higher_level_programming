a4f3654361b9e4b9d1c09c690c9998c52e025b7c
selehadin <selehadinseid1@gmail.com>
2022-09-22 10:05:49 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
+
+        i += 1

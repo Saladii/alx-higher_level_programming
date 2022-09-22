ce5bb98806fcd41e71266d763614c0234dd8a26d
selehadin <selehadinseid1@gmail.com>
2022-09-22 06:29:46 -0700

    update

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+i = 0
+j = 0
+while i < 10:
+    j = 0
+    while j < 10:
+        if (i != j and i < j):
+            print("{0:d}{0:d}".format(i, j), end"")
+            if i >= 8 and i <= 9:
+                print()
+            else:
+                print(end", ")
+                j += 1
+                i +=1

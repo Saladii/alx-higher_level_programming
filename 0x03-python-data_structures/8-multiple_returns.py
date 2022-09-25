7a63d4b2bd103af16374401bdbfcd025898a59c3
selehadin <selehadinseid1@gmail.com>
2022-09-25 02:20:32 -0700

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     k = len(sentence)
     if len(sentence) == 0:
-        return (None)
+        return (k, None)
     else:
         for c in sentence:
             return (k, sentence[0])

444b11550f7287523158efb3f26acc15d9053b35
selehadin <selehadinseid1@gmail.com>
2022-09-25 01:35:38 -0700

    update

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuble_a) < 2:
+        if len(tuble_a) == 0:
+            tuble_a = (0, 0)
+        else:
+            tuble_a = (tuble_a[0], 0)
+        if len(tuble_b) < 2:
+            tuble_b = (0, 0)
+        else:
+            tuble_b = (tuble_b[0], 0)
+        adetuble = (tuble_a[0] + tuble_b[0], tuble_a[1] + tuble_b[1])
+        return adetuble

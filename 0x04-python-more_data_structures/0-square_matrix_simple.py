895dffd5420dafa9e1f76f0c8f9a435fbea3f232
selehadin <selehadinseid1@gmail.com>
2022-10-01 03:51:36 -0700

    update

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,11 @@
+#!/bin/bash/python3
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for i in matrix:
+        new_row = sq_list(row)
+        new_list.append(new_row)
+    return new_list
+
+
+def sq_list(x):
+    return [i**2]

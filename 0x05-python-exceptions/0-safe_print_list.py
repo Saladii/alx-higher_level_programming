e81e53c930c827a3580f32a4f6eb1becfd48bcc7
selehadin <selehadinseid1@gmail.com>
2022-10-15 00:42:46 -0700

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     real = 0
     try:
         for i in range(x):
-            print(my_list=[], end="")
+            print(my_list[i], end="")
             real += 1
     except IndexError:
         pass

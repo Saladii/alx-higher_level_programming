943cd2d083e08dcb4292d780050cd3c139758850
selehadin <selehadinseid1@gmail.com>
2022-10-15 00:34:24 -0700

    update

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    real = 0
+    try:
+        for i in range(x):
+            print(my_list=[], end="")
+            real += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return real

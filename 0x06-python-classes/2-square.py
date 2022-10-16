0db4484715679e2baa0e4b1c5a9511f953238ac2
selehadin <selehadinseid1@gmail.com>
2022-10-16 03:48:22 -0700

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -13,4 +13,4 @@ class Square():
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        self._size = size
+        self.__size = size

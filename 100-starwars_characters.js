ed59273d4e6d5695d2eef9ecfad5fc019cbb3fd9
Selehadin <selehadinseid1@gmail.com>
2023-07-25 14:45:56 +0000

    update

---
+++ 100-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  JSON.parse(body).characters.forEach(function (url, callback) {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  });
+});
